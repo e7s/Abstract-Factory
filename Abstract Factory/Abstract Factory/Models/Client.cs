56d79931f64e065414f239cbef5288bf557fc814
eis <eis3g7@gmail.com>
2020-11-29 00:15:43 +0400

    Abstract class

---
+++ Abstract Factory/Abstract Factory/Models/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory.Models
+{
+    class Client
+    {
+        private AbstractWater water;
+        private AbstractBottle bottle;
+
+        public Client(AbstractFactory factory)
+        {
+            //Абстрагирование процессов инстанцирования
+            water = factory.CreateWater();
+            bottle = factory.CreateBottle();
+        }
+
+        public void Run()
+        {
+            //Абстагирование вариантов использования
+            bottle.Interact(water);
+        }
+    }
+}
